b31cae2be0fdeba3ed1474e06ff31116463748cd
Eloy Durán <eloy.de.enige@gmail.com>
2020-02-06 16:18:50 +0100

    make manual change, perhaps this could be made explicit in Flow source

--- typescript/flow-to-ts/Libraries/TurboModule/TurboModuleRegistry.tsx
+++ typescript/flow-to-ts/Libraries/TurboModule/TurboModuleRegistry.tsx
@@ -24,7 +24,7 @@ export function get<T extends TurboModule>(name: string): T | null | undefined {
 }
 
 export function getEnforcing<T extends TurboModule>(name: string): T {
-  const module = get(name);
+  const module = get<T>(name);
   invariant(module != null, `TurboModuleRegistry.getEnforcing(...): '${name}' could not be found. ` + 'Verify that a module by this name is registered in the native binary.');
   return module;
 }
